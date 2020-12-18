810eb4f6e302635fa1dccf85d2893e7de4924373
Ossamoon <osamu950623@gmail.com>
2020-12-18 22:41:32 +0900

    create Xcode project

---
+++ HapticMetronome/HapticMetronome/HapticMetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticMetronomeApp.swift
+//  HapticMetronome
+//
+//  Created by 齋藤修 on 2020/12/18.
+//
+
+import SwiftUI
+
+@main
+struct HapticMetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
